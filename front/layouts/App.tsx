5320ed5efdb05471d69bddead0080e4f44d28951
will kim <78planet@gmail.com>
2023-10-14 14:44:28 +0900

    add front

---
+++ front/layouts/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import loadable from '@loadable/component';
+import { Routes, Route } from 'react-router-dom';
+
+const SignUp = loadable(() => import('@pages/SignUp'));
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<SignUp />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  );
+};
+
+export default App;
---
+++ front/pages/SignUp/index.tsx
@@ -0,0 +1,16 @@
+import React, { useCallback, useState } from 'react';
+import { Header, Button, Error, Form, Input, Label, LinkContainer, Success } from '@pages/SignUp/styles';
+
+const SignUp = () => {
+
+  return (
+    <div id="container">
+      <Header>myreact</Header>
+      <Form>
+        <h1>hi</h1>
+      </Form>
+    </div>
+  );
+};
+
+export default SignUp;
